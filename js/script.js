80ac4b2ad414cd27944214396d8e3e1075601724
LorenzoPranio <lorenzopranio02@gmail.com>
2024-07-05 15:50:24 +0200

    [UPD]Start event listener function in script.js

--- js/script.js
+++ js/script.js
@@ -8,20 +8,26 @@ const grid = document.getElementById('grid')
 
 //Assegno l'evento click al pulsante recuperato nel dom 
 
+btn.addEventListener('click', function(){
+
+    //Eseguo un ciclo for per la grglia 10x10
+
+        //Creo un singolo quadrato funzione createsquare
+
+        //Aggiungo l'evento click al quadrato
+
+        //Quando cliccata diventa azzurra e mostra il numero in console
+
+    //Metto il numero progressivo al quadrato
+
+    //Appendo il quadrato creato nella griglia
+})
+
 //Definisco la funzione che mi crea i quadrati nella griglia
 
     //Assegnazione della classe square all'elemento creato
 
     //restituisco il quadrato nella variabile current element
 
-//Eseguo un ciclo for per la grglia 10x10
 
-    //Creo un singolo quadrato funzione createsquare
 
-    //Aggiungo l'evento click al quadrato
-
-    //Quando cliccata diventa azzurra e mostra il numero in console
-
-//Metto il numero progressivo al quadrato
-
-//Appendo il quadrato creato nella griglia
