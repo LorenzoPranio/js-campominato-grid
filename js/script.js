55d49293c992ec98016cba1b45efe74d62ebc934
LorenzoPranio <lorenzopranio02@gmail.com>
2024-07-05 15:02:30 +0200

    [UPD]Added scaffolding in index.html and script.js

---
+++ js/script.js
@@ -0,0 +1,23 @@
+//Recupero il pulsante dal dom
+
+//Recupero l'elemento della griglia
+
+//Assegno l'evento click al pulsante recuperato nel dom 
+
+//Definisco la funzione che mi crea i quadrati nella griglia
+
+    //Assegnazione della classe square all'elemento creato
+
+    //restituisco il quadrato nella variabile current element
+
+//Eseguo un ciclo for per la grglia 10x10
+
+    //Creo un singolo quadrato funzione createsquare
+
+    //Aggiungo l'evento click al quadrato
+
+    //Quando cliccata diventa azzurra e mostra il numero in console
+
+//Metto il numero progressivo al quadrato
+
+//Appendo il quadrato creato nella griglia
